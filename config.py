eb123a04341299d4fc92b50e39609d297cfd8bef
Jasurbekuzfx <jsadriddinov93@gmail.com>
2026-05-12 17:54:34 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 TOKEN = "8519404160:AAHDHkIltINWKtLRnjYHH9mYGM5hh-Z7214"
-CHANNELS = ["-1002016335962"]  # Kanal ID raqami (Silka emas, aynan ID kerak)
-ADMINS = [632426863] # O'zingizning Telegram ID raqamingiz
+ADMINS = [632426863]  # O'zingizning Telegram ID raqamingiz
+CHANNELS = ["-1002016335962"] # Kanal ID raqami
+INVITE_LINK = "https://t.me/+SrertxiwC0Y0ZDhi"
