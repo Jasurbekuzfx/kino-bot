a7346a89fd6005f570075dd16357c72a08d9e6c1
Jasurbekuzfx <jsadriddinov93@gmail.com>
2026-05-12 17:53:26 +0500

    Add Telegram bot configuration to config.py
    Added configuration for Telegram bot including token, channels, and admin IDs.

---
+++ config.py
@@ -0,0 +1,3 @@
+TOKEN = "8519404160:AAHDHkIltINWKtLRnjYHH9mYGM5hh-Z7214"
+CHANNELS = ["-1002016335962"]  # Kanal ID raqami (Silka emas, aynan ID kerak)
+ADMINS = [632426863] # O'zingizning Telegram ID raqamingiz
