a01860f67129b691f55c16ed16408c53d2840443
Jasurbekuzfx <jsadriddinov93@gmail.com>
2026-05-12 19:16:34 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-TOKEN = "8519404160:AAHDHkIltINWKtLRnjYHH9mYGM5hh-Z7214"
-ADMINS = [632426863]  # O'zingizning Telegram ID raqamingiz
-CHANNELS = ["-1002016335962"] # Kanal ID raqami
+import os
+
+# Render'dagi Environment Variables'dan o'qiydi
+TOKEN = os.getenv("BOT_TOKEN", "8519404160:AAHDHkIltINWKtLRnjYHH9mYGM5hh-Z7214")
+ADMINS = [632426863]
+CHANNELS = ["-1002016335962"]
 INVITE_LINK = "https://t.me/+SrertxiwC0Y0ZDhi"
