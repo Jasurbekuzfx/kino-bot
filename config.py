e5347ca7642efb53d5276c7098f6e0ae0db1cda1
Jasurbekuzfx <jsadriddinov93@gmail.com>
2026-05-13 09:46:09 +0500

    Update admin user ID in config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 # Render'dagi Environment Variables'dan o'qiydi
 TOKEN = os.getenv("BOT_TOKEN", "8519404160:AAHDHkIltINWKtLRnjYHH9mYGM5hh-Z7214")
-ADMINS = [632426863]
+ADMINS = [8225823974]
 
 # Siz bergan kanal ID raqami
 CHANNELS = ["-1003967862553"] 
