a35ec86ab28467b8186c24b89ca0043cf46cfb98
Jasurbekuzfx <jsadriddinov93@gmail.com>
2026-05-13 13:15:15 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 import os
 
-# Render'dagi Environment Variables'dan o'qiydi
 TOKEN = os.getenv("BOT_TOKEN", "8519404160:AAHDHkIltINWKtLRnjYHH9mYGM5hh-Z7214")
 ADMINS = [8225823974]
 
-# Kanallar va ularning maxsus taklif linklari
+# Kanallar va ularning maxsus linklari
 CHANNELS = {
     "-1003967862553": "https://t.me/+9_Pfk26VshtlNTJi",
     "-1003958419654": "https://t.me/+N213bsqhJnRkNDNi"
