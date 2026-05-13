6ea36673afccf111bd3cc5aa8ff8d7af59de005d
Jasurbekuzfx <jsadriddinov93@gmail.com>
2026-05-13 10:36:08 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import os
 
-# Render'dagi Environment Variables'dan o'qiydi
 TOKEN = os.getenv("BOT_TOKEN", "8519404160:AAHDHkIltINWKtLRnjYHH9mYGM5hh-Z7214")
-ADMINS = [8225823974] # Yangi Admin ID
+ADMINS = [8225823974]
 
-# Kanallarni bitta ro'yxatga jamlaymiz
-CHANNELS = ["-1003967862553", "-1003958419654"]
-
-# Taklif havolasi (birinchi yoki asosiy kanal uchun)
-INVITE_LINK = "https://t.me/+9_Pfk26VshtlNTJi"
+# Kanallar va ularning linklarini lug'at (dict) ko'rinishida yozamiz
+CHANNELS = {
+    "-1003967862553": "https://t.me/+9_Pfk26VshtlNTJi",
+    "-1003958419654": "https://t.me/+S-bSyourLink2" # Bu yerga 2-kanal linkini qo'ying
+}
