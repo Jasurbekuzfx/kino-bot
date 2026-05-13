efcb3626a6e4310316929823dc569d08f589e73d
Jasurbekuzfx <jsadriddinov93@gmail.com>
2026-05-13 10:40:34 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 TOKEN = os.getenv("BOT_TOKEN", "8519404160:AAHDHkIltINWKtLRnjYHH9mYGM5hh-Z7214")
 ADMINS = [8225823974]
 
-# Kanallar va ularning linklarini lug'at (dict) ko'rinishida yozamiz
+# Kanallar va ularning linklari
 CHANNELS = {
     "-1003967862553": "https://t.me/+9_Pfk26VshtlNTJi",
-    "-1003958419654": "https://t.me/+S-bSyourLink2" # Bu yerga 2-kanal linkini qo'ying
+    "-1003958419654": "https://t.me/+9_Pfk26VshtlNTJi" # 2-kanal linkini kiriting
 }
