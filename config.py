08c04e4d8b999f20298a7862f53e26651731a04a
Jasurbekuzfx <jsadriddinov93@gmail.com>
2026-05-13 09:13:13 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,9 @@ import os
 # Render'dagi Environment Variables'dan o'qiydi
 TOKEN = os.getenv("BOT_TOKEN", "8519404160:AAHDHkIltINWKtLRnjYHH9mYGM5hh-Z7214")
 ADMINS = [632426863]
-CHANNELS = ["-1002016335962"]
-INVITE_LINK = "https://t.me/+SrertxiwC0Y0ZDhi"
+
+# Siz bergan kanal ID raqami
+CHANNELS = ["-1003967862553"] 
+
+# Yangi almashtirilgan taklif havola
+INVITE_LINK = "https://t.me/+9_Pfk26VshtlNTJi"
